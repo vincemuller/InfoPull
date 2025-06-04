0521561c85037f5aaab5108873d7ea90ddbb7cdb
vincemuller <vmuller2529@gmail.com>
2025-06-04 15:07:46 -0700

    Initial Commit

---
+++ InfoPull/InfoPullApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfoPullApp.swift
+//  InfoPull
+//
+//  Created by Vince Muller on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct InfoPullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
